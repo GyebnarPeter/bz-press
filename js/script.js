0651fbdba6d9f81381437961421cbbb2a2c1ec3b
GyebnarPeter <gyebnarpetter@gmail.com>
2021-06-24 13:02:43 +0200

    correct navigation errors v.2

--- js/script.js
+++ js/script.js
@@ -3,11 +3,5 @@ const nav = document.querySelector(".nav");
 const navOpenButton = document.querySelector(".nav-open-button");
 const navCloseButton = document.querySelector(".nav__close-button img");
 
-navOpenButton.onclick = () => {
-    nav.style.opacity = "1";
-    nav.style.transform = "translateY(0)";
-};
-navCloseButton.onclick = () => {
-    nav.style.transform = "translateY(-100%)";
-    nav.style.opacity = "0";
-};
+navOpenButton.onclick = () => nav.style.top = "0";
+navCloseButton.onclick = () => nav.style.top = "-100%";
